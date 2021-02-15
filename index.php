ce401c87eccd8646a95716c1e9145f7b559af1bc
pragmatic335 <pragmatik335@gmail.com>
2021-02-16 01:54:42 +0700

    one more example late static bindings

--- index.php
+++ index.php
@@ -1,21 +1,42 @@
 <?php
-
+//one more example late static bindings
 abstract class DomainObject
 {
+    private $group;
+    public function __construct()
+    {
+        $this->group = static::getGroup();
+    }
+
+    public static function getGroup(): string
+    {
+        return 'default';
+    }
+
     public static function create()
     {
         return new static(); //example PHP-late-static-bindings
     }
 }
 
+
+class User extends DomainObject
+{
+
+}
+
 class Document extends DomainObject
 {
-
+    public static function getGroup(): string
+    {
+        return 'Documents';
+    }
 }
 
-class Dir extends DomainObject
+class SpreadSheet extends Document
 {
 
 }
 
-var_dump(Dir::create());
+var_dump(User::create());
+var_dump(SpreadSheet::create());
